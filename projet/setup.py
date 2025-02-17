d9589367dd27b02848ef7338e54fb60685ee7bd7
Clément Tourte <yanquille92@gmail.com>
2025-02-17 14:57:19 +0100

    aaaaah

--- projet/setup.py
+++ projet/setup.py
@@ -1,10 +1,5 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    content = f.readlines()
-requirements = [x.strip() for x in content]
-
 setup(name='toto',
       description="package description",
-      packages=find_packages(),
-      install_requires=requirements)
+      packages=find_packages())
