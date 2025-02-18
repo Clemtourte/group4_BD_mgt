15bbd777ffed9ef8eeb238bde992add8368504d4
Clement Tourte <yanquille92@gmail.com>
2025-02-18 13:35:18 +0100

    modified setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # Lire les dépendances depuis requirements.txt
 with open('requirements.txt') as f:
-    content = f.readlines()
-requirements = [x.strip() for x in content]
+    requirements = [line.strip() for line in f if line.strip()]
 
 setup(
     name='bdm_analysis',
+    version="0.1.0",  # Ajout de la version
     description="Business Data Management Analysis package",
-    packages=["bdm_analysis"],
+    packages=find_packages(),  # Recherche automatiquement les sous-packages
     install_requires=requirements
 )
